1b1478738db6a663071ad4565cf0c4f095ee101d
chin_poey_ong@dell.com <chin_poey_ong@dell.com>
2019-09-02 21:02:50 +0800

    firstpage merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { FirstPageComponent } from './first-page/first-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: FirstPageComponent},
+  {path: "second-page", component: SecondPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
 import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstPageComponent
+    FirstPageComponent,
+    SecondPageComponent
   ],
   imports: [
     BrowserModule,
