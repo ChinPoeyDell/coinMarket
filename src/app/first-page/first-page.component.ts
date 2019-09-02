9c011b622c60795e28d2647c3dd6c89e0a219f52
chin_poey_ong@dell.com <chin_poey_ong@dell.com>
2019-09-02 20:50:52 +0800

    firstpage completed

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-first-page',
@@ -8,6 +10,32 @@ import { Component, OnInit } from '@angular/core';
 export class FirstPageComponent implements OnInit {
 
   constructor() { }
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [10, 20, 30], label: 'Series 1' },
+    { data: [5, 30, 15], label: 'Series 2' },
+  ];
+  public lineChartLabels: Label[] = ['x', 'y', 'z'];
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
 
   ngOnInit() {
   }
